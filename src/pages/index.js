7694ce35a90afbb3c9ece9f9f12c7195856263de
rasheedsaqib <mrasheed.bese19seecs@seecs.edu.pk>
2023-01-03 04:05:31 +0500

    fix: changed styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -95,7 +95,7 @@ const Home = ({data}) => {
                 </div>
             </main>
 
-            <div className="relative overflow-hidden bg-white py-16">
+            <div className="relative overflow-hidden py-16">
                 <div className="hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full lg:[overflow-anchor:none]">
                     <div className="relative mx-auto h-full max-w-prose text-lg" aria-hidden="true">
                         <svg
